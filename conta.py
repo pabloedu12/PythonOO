1578e57850a2bd5df1721e720ce8a7af66c7662f
pabloedu17 <pe.eduardo123@gmail.com>
2023-04-27 13:15:42 -0400

    Usando métodos

--- conta.py
+++ conta.py
@@ -1,3 +1,17 @@
 class Conta:
 
-    pass
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo um objeto... {}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+    
+    def extrato(self):
+        print(f'Saldo de {self.saldo} do titular {self.titular}')
+
+    def deposita(self, valor):
+        self.saldo += valor 
+
+    def saca(self, valor):
+        self.saldo -= valor
